5b30acf8f019f3dabc4a57161e4240898be4b685
Aldrin Bacud <aldrinbacud@gmail.com>
2020-10-16 15:01:34 +0800

    Updates

---
+++ precache-manifest.b4a018bece6f75033cf8351b85800ab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c607a070d7f098bf352dd8ced1859182",
+    "url": "/aldrinbacud22/Monster.git/index.html"
+  },
+  {
+    "revision": "cc7dd6da39d804de71a7",
+    "url": "/aldrinbacud22/Monster.git/static/css/main.b9fa7772.chunk.css"
+  },
+  {
+    "revision": "6eb705f385370e863f05",
+    "url": "/aldrinbacud22/Monster.git/static/js/2.ab6a2843.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/aldrinbacud22/Monster.git/static/js/2.ab6a2843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7dd6da39d804de71a7",
+    "url": "/aldrinbacud22/Monster.git/static/js/main.130dbfda.chunk.js"
+  },
+  {
+    "revision": "5f6865344f10d8745372",
+    "url": "/aldrinbacud22/Monster.git/static/js/runtime-main.f3a0cd4c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/aldrinbacud22/Monster.git/precache-manifest.b4a018bece6f75033cf8351b85800ab6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aldrinbacud22/Monster.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
